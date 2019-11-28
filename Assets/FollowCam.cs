fe2da5a1b9dc654e7a3dcf157a897071a108cc12
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-27 21:15:27 -0300

    Main camera now keeps the Ground always in sight.

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -7,6 +7,10 @@ public class FollowCam : MonoBehaviour
     // The static point of interest.
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
     // The desired Z pos of the camera.
     public float camZ;
@@ -23,9 +27,16 @@ public class FollowCam : MonoBehaviour
 
         // Get the position of the POI.
         Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values.
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination.
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Force destination.z to be camZ to keep the camera far enough away.
         destination.z = camZ;
         // Set the camera to the destination.
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep the Ground in view.
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
