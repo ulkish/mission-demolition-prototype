185a30dbb3f9c17c180f22bc0a5c4ecb489d4f5d
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-28 22:57:59 -0300

    When the goal is reached it now turns bright green.

---
+++ Assets/__Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // A static field accessible by code anywhere.
+    static public bool goalMet = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // When the trigger's hit by something check to see if its a projectile.
+        if (other.gameObject.tag == "Projectile")
+        {
+            // If so, set goalMet to true.
+            Goal.goalMet = true;
+            // Also set the alpha of the color to higher opacity.
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+}
